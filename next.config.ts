f9b4e4020096d9e6d99898a4e230344089e896f6
Umair802 <farooqumair802@gmail.com>
2026-06-06 06:30:17 +0500

    feat: add RootLayout with metadata, configure server external packages, and migrate from yarn to npm

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["mongoose", "jsonwebtoken", "bcryptjs"],
 };
 
 export default nextConfig;
