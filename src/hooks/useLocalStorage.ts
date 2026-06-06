e630a4d5cb30d7099a3dc470b447cfad58d216d1
Umair802 <farooqumair802@gmail.com>
2026-06-06 06:36:24 +0500

    feat: add useLocalStorage hook to retrieve and parse data from cookies

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
 const useLocalStorage = (key: string): any => {
-  return Cookies.get(key) && JSON.parse(Cookies.get(key));
+  const [value, setValue] = useState<any>(null);
+
+  useEffect(() => {
+    const cookieValue = Cookies.get(key);
+    if (cookieValue) {
+      try {
+        setValue(JSON.parse(cookieValue));
+      } catch {
+        setValue(cookieValue);
+      }
+    }
+  }, [key]);
+
+  return value;
 };
 
 export default useLocalStorage;
